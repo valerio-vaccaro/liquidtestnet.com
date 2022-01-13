6d449b6c47c8640f909244b950be90717ffc5391
root <root@3n0z.c.time4vps.cloud>
2022-01-13 16:26:43 +0100

    call liquidtestnet api every 10 min

---
+++ clock.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+import requests 
+
+payload = str(datetime.utcnow()).encode("utf-8").hex()
+r = requests.get(f'https://liquidtestnet.com/api/utils?command=opreturn&text={payload}&action=send')
+print(r.json())
