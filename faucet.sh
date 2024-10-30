ea106b31c702df146f0afe67975c826dc6fc2bd3
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-10-30 10:30:02 +0000

    disable ipv6

--- faucet.sh
+++ faucet.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.tun0.disable_ipv6=1
+
 . venv3/bin/activate
 python faucet.py
 deactivate
